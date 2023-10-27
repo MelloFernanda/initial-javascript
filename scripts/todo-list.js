5444d7ab88b80b31521373dcf84beca7d1c17feb
Fernanda Batalha de Mello <fernandam04@outlook.com>
2023-10-27 11:33:15 -0300

    improving the todo-list file code, with .forEach and arrow function

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -22,17 +22,27 @@ function renderTodoList(){
         const html = `
             <div>${name}</div>
             <div>${dueDate}</div>
-            <button onclick="
-                todoList.splice(${index}, 1);
-                renderTodoList();
-            " class="delete-todo-button">Delete</button>
+            <button class="delete-todo-button js-delete-todo-button">Delete</button>
             `;
 
         todoListHMTL += html;
     });
 
     document.querySelector('.js-todo-list').innerHTML=todoListHMTL;
+
+    document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index)=>{
+        deleteButton.addEventListener('click', ()=>{
+            todoList.splice(index, 1);
+            renderTodoList();
+        });
+    });
 }
+
+
+document.querySelector('.js-add-todo-button').addEventListener('click', ()=>{
+    addTodo();
+})
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
